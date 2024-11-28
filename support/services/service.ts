7bda91156e4074db2c6bc904fe6c0213977bc02b
Rameesa <109940023+Rameesa96@users.noreply.github.com>
2024-11-28 16:24:13 +0530

    chore: tenant onboarding

--- support/services/service.ts
+++ support/services/service.ts
@@ -41,6 +41,7 @@ export const dbPath: Record<string, string> = {
 };
 
 export const pciiData: Record<string, unknown[]> = {
+  "295774345932832770": PCIITEST,
   "295760936122187778": PCIITESTED,
   "295760596131905538": PCIITESTING,
   "295760550514655234": PCIITEST,
